bd86cfb14bcc039760a5cef5a1b0108b173cba35
LisaAlisonDev <alisonbarretpro@gmail.com>
2023-02-17 22:26:16 +0400

    Added pagination to navigate between computers

--- gao/src/views/Computers.vue
+++ gao/src/views/Computers.vue
@@ -13,42 +13,68 @@ export default {
         return {
             path: mdiPlus,
             dialog: false,
+            page: 1,
+            maxPage: 0,
+            total: 0,
+            currentPage: 1,
             computerList: [] as Computer[]
         }
     },
     components: {
-		SvgIcon
-	},
+        SvgIcon
+    },
     // Methods are functions that mutate state and trigger updates.
     // They can be bound as event listeners in templates.
     methods: {
-        retrieveComputers() {
-            ComputerDataService.getAll()
+        retrieveComputers(page: number) {
+            ComputerDataService.getAll(page)
                 .then((response: ResponseData) => {
                     this.computerList = response.data.data.data;
+                    this.maxPage = response.data.data.last_page
+                    this.total = response.data.data.total
+                    this.currentPage = page
                     console.log(response.data.data.data);
                 })
                 .catch((e: Error) => {
                     console.log(e);
                 });
         },
-        refreshComputerList(value : Computer){
-            this.computerList.pop()
-            this.computerList.unshift(value)
+        refreshComputerList(value: Computer) {
+            this.retrieveComputers(this.currentPage)
+        },
+        changeToNextPage() {
+            this.currentPage++
+            if (this.currentPage < this.maxPage) {
+                this.retrieveComputers(this.currentPage)
+            }
+        },
+        changeToPreviousPage() {
+            this.currentPage--
+            if (this.currentPage < this.maxPage) {
+                this.retrieveComputers(this.currentPage)
+            }
         }
     },
     // Lifecycle hooks are called at different stages
     // of a component's lifecycle.
     // This function will be called when the component is mounted.
     mounted() {
-        this.retrieveComputers()
+        this.retrieveComputers(this.page)
     }
 }
 </script>
 
 <template>
     <main>
-        <AddComputer @computer="refreshComputerList"/> 
+        <div class="computer_pagination">
+            <v-btn @click="changeToPreviousPage()" v-if="currentPage !== 1" class="pagination_number">&lt;</v-btn>
+            <v-btn @click="retrieveComputers(n)" :active="n == currentPage" v-for="n in maxPage" class="pagination_number">
+                {{ n }}
+            </v-btn>
+            <v-btn @click="changeToNextPage()" v-if="currentPage !== maxPage" class="pagination_number">Suivant</v-btn>
+        </div>
+        
+        <AddComputer @computer="refreshComputerList" />
         <div class="computers_box">
             <div v-for="(item, index) in computerList" :key="index" class="computer_box">
                 <ComputerBox :computer=item! />
@@ -67,7 +93,29 @@ h1 {
 h3 {
     font-size: 1.2rem;
 }
-.add-computer-btn{
+
+.computer_pagination {
+    width: 60%;
+    margin: auto;
+    text-align: center;
+}
+
+.current_page {
+    color: red !important;
+}
+
+.computer_pagination>.pagination_number {
+    margin-top: 25px;
+    display: inline-block;
+    margin-right: 10px;
+    height: 50px;
+    line-height: 25px;
+    background-color: white;
+    padding: 15px;
+    border: 1px solid black;
+}
+
+.add-computer-btn {
     background: #adbab6;
     border: 1px solid #000000;
     border-radius: 5px;
@@ -79,16 +127,20 @@ h3 {
     line-height: 50px;
     text-align: center;
 }
-.add-computer-txt, .icon {
+
+.add-computer-txt,
+.icon {
     color: #fafdfc;
     display: inline-flex;
     align-items: center;
     vertical-align: middle;
 
 }
+
 div.add-computer-btn {
     display: inline-block;
 }
+
 .computers_box {
     display: inline-flex;
     margin-top: 20px;
@@ -104,5 +156,4 @@ div.add-computer-btn {
     margin-inline: 10px;
 }
 
-@media (min-width: 1024px) {}
-</style>
+@media (min-width: 1024px) {}</style>
